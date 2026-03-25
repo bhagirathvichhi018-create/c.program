f8c1ae8c70c3ee8249aa4d0a329fcb650eed7197
Bhagirath Vichhi <bhagirathvichhi018@gmail.com>
2026-03-25 18:07:51 +0530

    Add program to calculate perimeter of a circle

---
+++ Ch-2.Perimeter of Circle.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    float r, perimeter;
+
+    clrscr();
+
+    printf("Enter radius: ");
+    scanf("%f", &r);
+
+    perimeter = 2 * 3.14 * r;
+
+    printf("Perimeter of circle = %f", perimeter);
+
+    getch();
+}
