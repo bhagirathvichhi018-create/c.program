9877965494569a5e6b486a7c400cda3116ebe7f8
Bhagirath Vichhi <bhagirathvichhi018@gmail.com>
2026-03-25 18:05:43 +0530

    Add Simple Interest

---
+++ Ch-2.Simple Interest.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    float p, r, n, si;
+
+    clrscr();
+
+    printf("Enter principal: ");
+    scanf("%f", &p);
+
+    printf("Enter rate: ");
+    scanf("%f", &r);
+
+    printf("Enter time: ");
+    scanf("%f", &n);
+
+    si = (p * r * n) / 100;
+
+    printf("Simple Interest = %f", si);
+
+    getch();
+}
