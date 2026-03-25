2647fba26eb4d013eb8fb8c544bc4603b3185d3b
Bhagirath Vichhi <bhagirathvichhi018@gmail.com>
2026-03-25 10:42:19 +0530

    Add files via upload

---
+++ CH1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+  int main()
+ {
+   clrscr();
+   printf("Name: Bhagirath\n");
+   printf("School: Iswaranand vidhyaly\n");
+   printf("Age: 26\n");
+    return 0;
+
+
+
+
+
+
+ }
