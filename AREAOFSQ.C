4b3a8dda901261fe0707da92629d73761b952210
Bhagirath Vichhi <bhagirathvichhi018@gmail.com>
2026-03-25 10:30:55 +0530

    Add files via upload

---
+++ AREAOFSQ.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+ main()
+ {
+   float l, area;
+   clrscr();
+   printf("Enter length;");
+   scanf("%f", &l);
+   area = l * l;
+   printf("Area of square = %f", area);
+   getch();
+
+
+
+
+
+
+
+
+ }
